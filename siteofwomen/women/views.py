eef2e7cfadc8c03c32a021eebbe8d98378f286e6
SaulGoodman228 <daniil.shapin327@gmail.com>
2024-02-27 17:39:22 +0300

    First filters

--- siteofwomen/women/views.py
+++ siteofwomen/women/views.py
@@ -2,13 +2,25 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 from django.template.loader import render_to_string
 
+menu=['О сайте','Добавить статью','Обратная связь','Войти']
 
+class MyClass:
+    def __init__(self,v1,v2):
+        self.a=v1
+        self.b=v2
 
 
 def main_paige(request):
     # t=render_to_string('women/index.html')
     # return HttpResponse(t)
-    data={'title':'Главная страница'}
+    data={'title':'Главная страница',
+          'menu':menu,
+          'float':28.56,
+          'lst':[1,2,'abc',True],
+          'set':{1,2,3,4,5},
+          'dict':{'key_1':'value_1','key_2':'value_2'},
+          'obj':MyClass(10,20),
+          }
     return render(request,'women/index.html',data)
 
 def index(request): #HttpRequest
