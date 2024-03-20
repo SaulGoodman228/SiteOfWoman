0d911377f07bcbc6d73ce433beb49f0b922a9dd5
SaulGoodman228 <daniil.shapin327@gmail.com>
2024-03-20 09:43:49 +0300

    Commit tables

--- siteofwomen/women/models.py
+++ siteofwomen/women/models.py
@@ -8,33 +8,43 @@ class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(is_published=Women.Status.PUBLISHED)
 
+
 class Women(models.Model):
     class Status(models.IntegerChoices):
-        DRAFT=0,'Черновик'
-        PUBLISHED=1,'Опубликованно'
+        DRAFT = 0, 'Черновик'
+        PUBLISHED = 1, 'Опубликованно'
 
-    title = models.CharField(max_length= 255)
-    slug=models.SlugField(max_length=255, unique=True, db_index=True)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
     content = models.TextField(blank=True)
-    time_create = models.DateTimeField(auto_now_add= True)
-    time_update = models.DateTimeField(auto_now= True)
-    is_published = models.BooleanField(choices=Status.choices,default=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(choices=Status.choices, default=True)
+    cat = models.ForeignKey('Category',on_delete=models.PROTECT,related_name='posts')
 
     def __str__(self):
         return self.title
-    #Отвечает за отображение в опред Виде
+    # Отвечает за отображение в опред Виде
 
-    objects=models.Manager()
-    published=PublishedManager()
-    #Контекстный менеджер
+    objects = models.Manager()
+    published = PublishedManager()
+    # Контекстный менеджер
 
     class Meta:
-        ordering =['-time_create']
+        ordering = ['-time_create']
         indexes = [
             models.Index(fields=['-time_create'])
         ]
-        #Ordering - отвечает как по стандарту будет сортироваться дб
-        #indexes - индексирует заданный нами параметр
+        # Ordering - отвечает как по стандарту будет сортироваться дб
+        # indexes - индексирует заданный нами параметр
 
     def get_absolute_url(self):
-        return reverse('post',kwargs={'post_slug':self.slug})
+        return reverse('post', kwargs={'post_slug': self.slug})
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
