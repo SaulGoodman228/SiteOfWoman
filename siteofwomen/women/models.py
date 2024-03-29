8fdf841064a5a257a89a9b992a36975094531ad9
SaulGoodman228 <daniil.shapin327@gmail.com>
2024-03-29 11:35:05 +0300

    tags_who can

--- siteofwomen/women/models.py
+++ siteofwomen/women/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.urls import reverse
 
 
-# Create your models here.
-
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(is_published=Women.Status.PUBLISHED)
@@ -57,9 +55,6 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_slug': self.slug})
 
-
-
-
 class TagPost(models.Model):
     tag = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
@@ -74,6 +69,7 @@ class TagPost(models.Model):
 class Husband(models.Model):
     name=models.CharField(max_length=100)
     age = models.IntegerField(null=True)
+    m_count= models.IntegerField(blank=True, default=0)
 
     def __str__(self):
         return self.name
