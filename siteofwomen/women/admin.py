eb014c534d2abb79b0b5ad2b3d8a0dd804546625
SaulGoodman228 <daniil.shapin327@gmail.com>
2024-04-04 20:58:21 +0300

    filter+custom admin panel

--- siteofwomen/women/admin.py
+++ siteofwomen/women/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import Women, Category
 
 # Register your models here.
@@ -6,20 +6,65 @@ from .models import Women, Category
 admin.site.site_header = 'Панель администрирования'
 admin.site.index_title = 'Известные женщины мира'
 
+#Создание пользовательского фильтра
+class MarriedFilter(admin.SimpleListFilter):
+    title = 'Status'
+    parameter_name = 'status'
+    def lookups(self, request, model_admin):
+        return[
+            ('married','Замужняя'),
+            ('single','Не замужняя'),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'married':
+            return queryset.filter(husband__isnull=False)
+        elif self.value() == 'single':
+            return queryset.filter(husband__isnull=True)
+        else:
+            return queryset.all()
+
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
     #отображение нужных полей
-    list_display = ('id','title','time_create','is_published','cat')
-
-    #кликабельность поля
-    list_display_links = ('id',)
-
+    list_display = ('title','time_create','is_published','cat', 'brief_info')
+    list_display_links = ('title',)
     #указание сортировки полей только для админ панели
-    ordering = ['time_create','title']
-
-    list_editable = ('is_published','title','cat')
-
+    ordering = ['time_create']
+    list_editable = ('is_published','cat')
     list_per_page = 5
+    #Добавление пользовательских действий в список
+    actions=('set_published','set_draft')
+    #позволяет проводить поиск по выбранным полям
+    search_fields = ['title','cat__name']
+    #Добавление полей фильтрации
+    list_filter = [MarriedFilter,'cat__name','is_published']
+    #поля отоброжаемые в форме для редактирования
+    fields = ['title','content','slug','cat','husband']
+    # exclude делает то же только наоборот readonly_fields - делает поля нередактируемыми
+
+    #автоматически транслитирует поле по другому полю
+    prepopulated_fields = {'slug':('title',)}
+
+    #filter_horizontal = ['tags']
+
+
+
+    #Пользовательское поле
+    @admin.display(description='Краткое описание',ordering='content')
+    def brief_info(self, women: Women):
+        return f'Описание {len(women.content)}'
+
+    #Пользовательское действие
+    @admin.action(description='Опубликовать выбранные записи')
+    def set_published(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.PUBLISHED)
+        self.message_user(request,f'Измененно {count} записей')
+
+    @admin.action(description='Снять с публикации выбранные записи')
+    def set_draft(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.DRAFT)
+        self.message_user(request, f'{count} записей снято с публикации!', messages.WARNING)
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
--- siteofwomen/women/models.py
+++ siteofwomen/women/models.py
@@ -1,10 +1,21 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(is_published=Women.Status.PUBLISHED)
+
+
+def translit_to_eng(s: str) -> str:
+    d = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd',
+         'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i', 'к': 'k',
+         'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r',
+         'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch',
+         'ш': 'sh', 'щ': 'shch', 'ь': '', 'ы': 'y', 'ъ': '', 'э': 'r', 'ю': 'yu', 'я': 'ya'}
+
+    return "".join(map(lambda x: d[x] if d.get(x, False) else x, s.lower()))
 
 
 class Women(models.Model):
@@ -47,6 +58,10 @@ class Women(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
 
+    #Формируется slug на основе звголовка
+    # def save(self, *args, **kwargs):
+    #     self.slug=slugify(translit_to_eng(self.title))
+    #     super.save(*args, **kwargs)
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True)
